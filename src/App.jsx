f0e40c54bd48c9a971b93e140998b7fd4aee88c4
jeni-t <jenivijay19@gmail.com>
2025-06-12 06:20:04 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Resume from "./Resume";
 import Skills from "./Skills"
 import Project from "./Project"
 import Contact from "./Contact"
+import girlImage from '../public/jeni.jpeg';
 
 
 // function App() {
@@ -79,7 +80,7 @@ function App() {
     <div className="flex h-screen">
       {/* Sidebar - stays fixed */}
       <aside className="w-1/4 bg-[#0f172a] text-white p-6 flex flex-col items-center overflow-y-auto fixed h-screen">
-        <img src="/public/jeni.jpeg" className="w-28 h-28 rounded-full border-8 border-gray-600" />
+        <img src={girlImage} className="w-28 h-28 rounded-full border-8 border-gray-600" />
         <p className="py-4 font-bold text-2xl">Jenifer V</p>
 
         <SidebarLink to="/" label="Home" icon="home.jpg" />
